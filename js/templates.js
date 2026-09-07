75e0f93aa10249ef47b6be71929242f8e66857d7
Riko Andriawan <97824888+rikoandre@users.noreply.github.com>
2026-09-07 23:45:11 +0700

    Refactor OLT templates and remove deprecated entries
    Removed multiple database templates for OLT configurations and updated the formatting of existing templates.

--- js/templates.js
+++ js/templates.js
@@ -413,22 +413,19 @@ write`,
 
     "511": (d) => `conf t
 interface gpon-olt_${d.iface}
- onu ${d.onu} type ALL sn ${d.sn}
+  onu ${d.onu} type ALL sn ${d.sn}
 exit
 interface gpon-onu_${d.iface}:${d.onu}
- name ${d.user}
- description ${d.desc}
- sn-bind enable sn
- tcont 1 name PPPOE profile kusuma
- gemport 1 name PPPOE tcont 1
- switchport mode hybrid vport 1
- service-port 1 vport 1 user-vlan 511 vlan 511
+  name ${d.user}
+  description ${d.user} - ${d.desc}
+  tcont 1 profile kusuma
+  gemport 1 tcont 1
+  service-port 1 vport 1 user-vlan 511 vlan 511
 exit
 pon-onu-mng gpon-onu_${d.iface}:${d.onu}
- service ServiceName gemport 1 cos 0 vlan 511
- wan-ip 1 mode pppoe username ${d.user} password ${d.pass} vlan-profile 511 host 1
- wan-ip 1 ping-response enable traceroute-response enable
- security-mgmt 212 state enable mode forward protocol web
+  service 1 gemport 1 vlan 511
+  wan-ip 1 mode pppoe username ${d.user} password ${d.pass} vlan-profile v511 host 1
+  security-mgmt 1 state enable mode forward protocol web
 exit
 exit
 write`,
